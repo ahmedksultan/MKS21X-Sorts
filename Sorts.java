cec2ec9d4310518aea27e17a4c4e872642e6b407
asultan00 <asultan00@stuy.edu>
2018-12-04 23:45:17 -0500

    works

--- Sorts.java
+++ Sorts.java
@@ -3,21 +3,29 @@ import java.util.*;
 
 public class Sorts {
      public static void selectionSort(int[] input) {
+          int minval;
+          int tempval;
+          int index2;
+          //looping through whole array
           for (int i = 0; i < input.length; i++) {
-               //loops thru input array, and finds value with minimum index, sorts based on that
-               int minidx = i;
-               for (int q = i + 1; q < input.length; q++) {
-                    if (input[q] < input[minidx]) {
-                         minidx = q;
+               minval = input[i];
+               index2 = i;
+               //looping to find where to switch
+               for (int q = i; q < input.length; q++) {
+                    tempval = input[q];
+                    if (tempval < minval) {
+                         minval = tempval;
+                         index2 = q;
                     }
-                    //holds value and switches
-                    int hold = input[minidx];
-                    input[minidx] = input[q];
-                    input[q] = hold;
                }
+               int holdval = input[i];
+               input[i] = minval;
+               input[index2] = holdval;
           }
+
      }
      public static void main(String[] args) {
+          //testing
           int[] testary1 = {51, 3, 6, -4, 8, 99, 10, 1, 7};
           selectionSort(testary1);
           System.out.println(Arrays.toString(testary1));
