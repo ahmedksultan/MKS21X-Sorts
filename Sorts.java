90a72a0dc8b5157c2436e9a9ac7ee12225fac520
asultan00 <asultan00@stuy.edu>
2018-12-13 09:19:25 -0500

    forgot to push and edit earlier

--- Sorts.java
+++ Sorts.java
@@ -4,19 +4,32 @@ import java.util.*;
 public class Sorts {
      public static void main(String[] args) {
           //testing
-          int[] testary0s = {};
-          int[] testary0b = {};
+
           int[] testary1 = {51, 3, 6, -4, 8, 99, 10, 1, 7, 66, 44, 33, 22, 1, 88, 9, 7, 5, -43, 6, 7, 9};
           selectionSort(testary1);
           System.out.println(Arrays.toString(testary1));
+
           int[] testary2 = {51, 3, 6, -4, 8, 99, 10, 1, 7, 66, 44, 33, 22, 1, 88, 9, 7, 5, -43, 6, 7, 9};
           bubbleSort(testary2);
           System.out.println(Arrays.toString(testary2));
+
+          int[] testary3 = {51, 3, 6, -4, 8, 99, 10, 1, 7, 66, 44, 33, 22, 1, 88, 9, 7, 5, -43, 6, 7, 9};
+          insertionSort(testary3);
+          System.out.println(Arrays.toString(testary3));
+
           //testing empties
+          int[] testary0s = {};
+          int[] testary0b = {};
+          int[] testary0i = {};
+
           selectionSort(testary0s);
           System.out.println(Arrays.toString(testary0s));
+
           bubbleSort(testary0b);
           System.out.println(Arrays.toString(testary0b));
+
+          insertionSort(testary0i);
+          System.out.println(Arrays.toString(testary0i));
 
      }
 
@@ -45,7 +58,7 @@ public class Sorts {
 
      //bubbleSort
      public static void bubbleSort(int[] input) {
-          for(int i = input.length; i > 0; i--) {
+          for (int i = input.length; i > 0; i--) {
                for (int q = 0; q < i - 1; q++) {
                     if (input[q] > input[q+1]) {
                          int tempval = input[q];
@@ -56,4 +69,23 @@ public class Sorts {
           }
      }
 
+
+     //insertionSort
+     public static void insertionSort(int[] input) {
+          //Alma helped me with this
+          for (int i = 1; i < input.length; i++) {
+               int idx = i - 1;
+               int temp = input[i];
+               while (idx >= 0 && input[idx] > temp) {
+                    input[idx+1] = input[idx];
+                    idx--;
+               }
+               if (idx < 0) {
+                    input[0] = temp;
+               }
+               else {
+                    input[idx+1] = temp;
+               }
+          }
+     }
 }
